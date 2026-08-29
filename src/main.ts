c1923e09f6284143847c70f11a1865dac1e08632
Samad126 <samed192005@gmail.com>
2026-08-30 02:15:26 +0400

    feat: initialize database

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
+import path from 'node:path';
+import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+
+config({ path: path.resolve(process.cwd(), '../.env') });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
